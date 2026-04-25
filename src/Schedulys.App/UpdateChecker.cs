31825c2a2c76165ddc4a9ca8b70d99846afc2ab7
redmik0909 <redarahal0909@gmail.com>
2026-04-25 15:24:39 -0400

    Release v1.1

--- src/Schedulys.App/UpdateChecker.cs
+++ src/Schedulys.App/UpdateChecker.cs
@@ -11,7 +11,7 @@ namespace Schedulys.App;
 
 public static class UpdateChecker
 {
-    public const string CurrentVersion = "1.0";
+    public const string CurrentVersion = "1.1";
 
     private const string ApiUrl = "https://api.github.com/repos/redmik0909/Schedulys/releases/latest";
 
